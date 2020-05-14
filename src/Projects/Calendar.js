cf964b0f612aae03b7569613cdc12452be2f716e
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-05-14 01:54:57 -0400

    fixed typo in calendar

--- src/Projects/Calendar.js
+++ src/Projects/Calendar.js
@@ -8,7 +8,7 @@ function Calendar(){
             <div>Calendar</div>
             <br />
             <img src={calendar} alt="A picture of a calendar" />
-            <p>A Java project intended implement a calendar, with a variety of functions. This program allows user to keep track of any events in a more organized fashion, with buildt in alert system to remind the user of upcoming events. One cool feature is that this program also allows event sharing with other users.</p>
+            <p>A Java project intended to implement a calendar, with a variety of functions. This program allows users to keep track of any event in a more organized fashion, with a built in alert system to remind users of upcoming events. One cool feature is that this program also allows event sharing with other users.</p>
             <a href="https://github.com/StevenXu96/Calendar" target="_blank" rel="noopener noreferrer">View GitHub</a>
         </div>
     )
