dec432af34f9b5d3bb5f945df31149871a3d1985
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-05-14 01:10:13 -0400

    changed about me page

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,26 @@ function About(){
                 <h2>Who Am I?</h2>
                 <p>
                     My name is Steven Xu. I am a Computer Science student at the University of Toronto.
-                    languages I am familiar with: Java, Python, React.
                 </p>
+                <p>
+                    I also already completed a Bachelor of Science degree in Biochemistry at the University of British Columbia.
+                </p>
+                <br />
+                <h2>Languages I am familiar with:</h2>
+                <ul className="languages">
+                    <li>Python</li>
+                    <li>Java</li>
+                    <li>C, C++</li>
+                    <li>JavaScript</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                </ul>
+                <br />
+                <h2>Web frameworks I am familiar with:</h2>
+                <ul className="frameworks">
+                    <li>React</li>
+                    <li>django</li>
+                </ul>
             </div>
         </h1>
     )
