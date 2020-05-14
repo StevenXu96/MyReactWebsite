deb660040a1a3807a738dbb2371a068adc0bc85a
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-05-13 22:48:42 -0400

    Updates

---
+++ precache-manifest.46dadc7240fe1944787b0dafde7e9052.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53be7a56ed0670f56e481ecb0b0d1a3",
+    "url": "/MyReactWebsite/index.html"
+  },
+  {
+    "revision": "e0e90c62c3485262a939",
+    "url": "/MyReactWebsite/static/js/2.beb50654.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyReactWebsite/static/js/2.beb50654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfd4bdd112cc6146e54",
+    "url": "/MyReactWebsite/static/js/main.580800ad.chunk.js"
+  },
+  {
+    "revision": "8354289594199d0845fe",
+    "url": "/MyReactWebsite/static/js/runtime-main.5b1bfd82.js"
+  },
+  {
+    "revision": "50d89309250277e5ac2ddb73a5adb213",
+    "url": "/MyReactWebsite/static/media/Avalon.50d89309.png"
+  },
+  {
+    "revision": "303439f2751be4ab46d0e3cd25e755d3",
+    "url": "/MyReactWebsite/static/media/bittorrent.303439f2.png"
+  },
+  {
+    "revision": "8f3cea0ec0524d4d59dc163692ec9b67",
+    "url": "/MyReactWebsite/static/media/calendar.8f3cea0e.png"
+  }
+]);
