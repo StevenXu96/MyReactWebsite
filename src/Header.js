da5aeb41550dcae5330881f0984818383370b517
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-08-12 14:39:59 -0400

    trying out

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,6 @@ import React from 'react'
 
 function Header(){
     return(
-        /*
-        <header className="sticky">
-            <p>STEVEN'S PORTFOLIO</p>
-        </header>
-         */
         <header className="sticky">
             <p className='masthead-intro'>Hi I'm</p>
             <h1 className='masthead-heading'>Steven!</h1>
