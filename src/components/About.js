25efe43146d2e6601793f0d8f25de05fbefff116
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-08-30 14:09:00 -0400

    editted about me page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,22 @@ function About(){
         <div className="about_grid">
             <div className="About_Me">
                 <h3>About Me</h3>
-                <p>A WHOLE PARAGRAH, BLAH BLAH BLAH</p>
+                <p>Hi, My name is Steven. I am currently a Computer Science student at the University of Toronto. 
+                    I am passionate about programming, especially learning new languages to build new things, 
+                    such as this website! Currently, I am working on a creative full stack MERN Chat App which utilizes socket-io. 
+                    So far, I have completed 4 personal projects, ranging from full-stact web applications to Android Software 
+                    developments. One thing I have learnt from these projects is that I love learning new things, 
+                    and constantly improving myself. 
+                </p> <br />
+                <p>
+                    I've been enthusiastic about open source. One major comtribution I have made so far was when I was implementing 
+                    the NBA Analytic Tool. I was using an API service of an open source project. I contributed to that API service 
+                    to make the search function much more optimized by adding new search filters. 
+                </p> <br />
+                <p>
+                    Outside of programming related fields, I love sports. I am fairly good at tennis and basketball. I also love to travel, 
+                    Japan is my favourite destinaton. 
+                </p>
             </div>
             <div className="Picture">
                 <img src={StevenPic} alt="A picture of the awesome programmer Steven" />
@@ -15,8 +30,20 @@ function About(){
                 <h3>Skills</h3>
             </div>
             <div className="skill1">
+                <ol>
+                    <li>Java</li>
+                    <li>Python</li>
+                    <li>HTML, CSS</li>
+                    <li>JavaScript</li>
+                    <li>C/C++</li>
+                </ol>
             </div>
             <div className="skill2">
+            <ol>
+                    <li>ReactJs</li>
+                    <li>MERN</li>
+                    <li>Data Structures + Algorithms</li>
+                </ol>
             </div>
         </div>
     )
